ca6faf68c48cb08b79d1c35fea946ffd4c1950f9
sharmajaganathan <sharmajaganathan25@gmail.com>
2023-02-25 13:47:10 +0000

    git ignore files

--- demo.java
+++ demo.java
@@ -1,5 +1,7 @@
 public class demo{
+	//print statement for hello world
 	public static void main(string args[]){
+		system.ou.println("welcome to devops")
 		system.out.println("hello world")
 	}
 }
